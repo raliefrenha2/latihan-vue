ede80e8dca224b75f691828a7c400fb85b092354
ai-romi <raliefrenha2@gmail.com>
2019-07-18 21:51:39 +0700

    Component within component

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,26 @@
+Vue.component('task-list', {
+    template: `<div>
+                    <task v-for="task in tasks">{{ task.task}}</task>
+                </div>`,
+
+    data() {
+        return {
+            tasks: [
+                { task: 'Pai ka Pasa', complete: true },
+                { task: 'Pai ka mall', complete: false },
+                { task: 'Pai ka Apotik', complete: true },
+                { task: 'Pai Karajo', complete: false }
+            ]
+        }
+    }
+});
+
 Vue.component('task', {
     template: '<li><slot></slot></li>'
 });
 
+
 new Vue({
     el: '#app',
-    data: {
-        message: 'Hello World'
-    }
+
 })
