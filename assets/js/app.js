d558ea5e9a2892bd499c7ceedd80fa445f8ee443
ai-romi <raliefrenha2@gmail.com>
2019-07-18 13:13:54 +0700

    watch properties

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,7 +4,9 @@ new Vue({
         message: 'Vue Computed',
         int1: 2,
         int2: 3,
-        result: null
+        result: null,
+        kilometers: 0,
+        meters: 0,
     },
     computed: {
         sum: function () {
@@ -14,6 +16,16 @@ new Vue({
     methods: {
         sumProcess: function (int3) {
             return this.result = this.int1 + this.int2 + int3;
+        }
+    },
+    watch: {
+        kilometers: function (val) {
+            this.kilometers = val;
+            this.meters = val * 1000;
+        },
+        meters: function (val) {
+            this.kilometers = val / 1000;
+            this.meters = val;
         }
     }
 })
